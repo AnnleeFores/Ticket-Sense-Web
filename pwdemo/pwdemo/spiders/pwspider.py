96a4591c4211a6316e0f197fe0d800dcb25245bb
Annlee Fores <annleefores@gmail.com>
2022-07-10 13:23:11 +0530

    tk spider update

--- pwdemo/pwdemo/spiders/pwspider.py
+++ pwdemo/pwdemo/spiders/pwspider.py
@@ -1,13 +1,15 @@
+from fileinput import filename
 from requests import request
 import scrapy
 from scrapy_playwright.page import PageCoroutine
 
+film = 'love'
 
 class PwspiderSpider(scrapy.Spider):
     name = 'pwspider'
     
     def start_requests(self):
-        yield scrapy.Request('https://www.ticketnew.com/Carnival-Downtown--Thalassery-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/12539', 
+        yield scrapy.Request('https://www.ticketnew.com/INOX-Atria-Mall-Worli-Mumbai-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/12319/20220710', 
         meta=dict(
             playwright = True,
             playwright_include_page = True,
@@ -17,11 +19,15 @@ class PwspiderSpider(scrapy.Spider):
         ))
 
     async def parse(self, response):
-        venuehtml =  response.css('div#divTheatreInfo')
-        yield {
-            'venue' : venuehtml.css('h2::text').get(),
-        }
-        for show in response.css('div.tn-entity-details')[1:]:
-            yield {
-                'show' : show.css('h5::text').get(),
-            }
+        ## if date booking is active get the details
+        active = response.css('li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active').get()
+
+        if active != None:
+            venuehtml =  response.css('div#divTheatreInfo')
+            for show in response.css('div.tn-entity-details')[1:]:
+                showname = show.css('h5::text').get()
+                if film in showname.lower():
+                    yield {
+                        'venue' : venuehtml.css('h2::text').get(),
+                        'show' : showname,
+                    }
