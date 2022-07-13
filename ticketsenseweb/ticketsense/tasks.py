9eda29b5356916db0f68ab8f1c264fc45c1a6528
Annlee Fores <annleefores@gmail.com>
2022-07-13 14:31:00 +0530

    telegram messaging included

--- ticketsenseweb/ticketsense/tasks.py
+++ ticketsenseweb/ticketsense/tasks.py
@@ -4,34 +4,62 @@ from celery.utils.log import get_task_logger
 import requests
 from .models import Trigger
 
+# os module used for getting env variables
+import os
+# telegram module to interact with telegram chat app
+import telebot
+# used to source data from .env
+from dotenv import load_dotenv
+
+from time import sleep
+
 
 logger = get_task_logger(__name__)
 
+# loads dotenv to collect data from .env
+load_dotenv()
 
+# assigns value collected from .env to variables
+API_KEY_TEST = os.getenv('API_KEY_TEST')
+API_KEY = os.getenv('API_KEY')
+USER_ID = os.getenv('USER_ID')
 
+# code to initialize telegram bot function
+bot = telebot.TeleBot(API_KEY)
+@shared_task(ignore_result=True)
+def message(msg, pk):
+    trigger = Trigger.objects.get(id=pk)
+    trigger.delete()
+    for i in range(3):
+        bot.send_message(USER_ID, msg)
+        sleep(60)
+    
 
-#     link = 'INOX-Atria-Mall-Worli-Mumbai-Book-My-Movie-Show-Tickets/Online-Ticket-Booking/12319/20220712'
-#     filmkeyword = 'love'
-#     date = '2022-07-12'
-
+testbot = telebot.TeleBot(API_KEY_TEST)
+def testmessage(msg):
+    testbot.send_message(USER_ID, msg)
 
     
 @shared_task(ignore_result=True)
 def five_min_func():
 
-    triggers = Trigger.objects.all()
+    try:
+        triggers = Trigger.objects.all()
 
-    for trigger in triggers:
-        link = trigger.link
-        filmkeyword = trigger.film
-        date = (trigger.date).strftime('%Y-%m-%d')
-        site = trigger.site
-        fetch.delay(link, filmkeyword, date, site)
+        for trigger in triggers:
+            pk = trigger.id
+            link = trigger.link
+            filmkeyword = trigger.film
+            date = (trigger.date).strftime('%Y-%m-%d')
+            site = trigger.site
+            fetch.delay(link, filmkeyword, date, site, pk)
+    except:
+        pass
 
         
 
 @shared_task(ignore_result=True)
-def fetch(link, filmkeyword, date, site):
+def fetch(link, filmkeyword, date, site, pk):
     
     response = (requests.get(f'http://127.0.0.1:9080/crawl.json?spider_name={site}&start_requests=true&crawl_args={{"link":"{link}","film":"{filmkeyword}","date":"{date}"}}').json())
     try:
@@ -40,10 +68,7 @@ def fetch(link, filmkeyword, date, site):
         data = ''
 
     logger.info(data)
-    # if data != []:
-    #     for i in data:
-    #         logger.info(i['venue'])
-    #         logger.info(i['show'])
-    #         logger.info(i['date'])
-
+    if data != []:
+        for i in data:
+            message.delay(f'telegram working {i}', pk)
     return 'done'
