88b9dd0ff8d78976bde02e5adf6c64838cdccdcd
Annlee Fores <annleefores@gmail.com>
2022-07-21 15:59:25 +0530

    movie name search

--- tswfrontend/src/components/Main.jsx
+++ tswfrontend/src/components/Main.jsx
@@ -1,123 +1,113 @@
 import React from "react";
-import { useState } from "react";
-import { Calendar } from "@mantine/dates";
-import { Chip, Chips, MantineProvider } from "@mantine/core";
+import { useState, useEffect } from "react";
+import { DatePicker } from "@mantine/dates";
+import { SegmentedControl, Autocomplete } from "@mantine/core";
 import dayjs from "dayjs";
+import { Calendar, MasksTheater, Movie } from "tabler-icons-react";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+import axios from "axios";
+import requests from "../Requests";
+
+dayjs.extend(customParseFormat);
 
 const Main = () => {
-  const [value, setValue] = useState([]);
-  const [movie, setMovie] = useState([]);
-  const [theater, setTheater] = useState([]);
-  const [site, setSite] = useState("");
+  const [value, onChange] = useState(new Date());
+  const [movie, setMovie] = useState("");
+  const [moviedata, setMoviedata] = useState([]);
+  const [theater, setTheater] = useState("");
+  const [site, setSite] = useState("react");
 
   // convert output from calendar to date format
-  const dateFormat = value
-    .map((date) => dayjs(date).format("YYYY-MM-DD"))
-    .join(", ");
+  const dateFormat = dayjs(value).format("YYYY-MM-DD");
+
+  useEffect(() => {
+    if (movie !== "") {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=00e6af3c5f4640d75b94527d05ec7098&language=en-US&query=${movie}&page=1&include_adult=false`
+        )
+        .then((response) => {
+          const movietitle = response.data.results.map(
+            (item) =>
+              `${item?.title} - ${dayjs(item?.release_date).format("YYYY")}`
+          );
+
+          setMoviedata(movietitle);
+        });
+    } else {
+      setMoviedata([]);
+    }
+  }, [movie]);
 
   const log = async (e) => {
     e.preventDefault();
-    console.log(movie);
+    console.log(["aa", "aala"]);
     console.log(theater);
     console.log(site);
     console.log(dateFormat);
-    setMovie([]);
+    console.log(moviedata);
+    setMovie("");
+    setMoviedata([]);
     setTheater([]);
-    setValue([]);
-
-    e.target.reset();
+    onChange(new Date());
   };
 
   return (
     <div className="w-full h-[500px] text-white">
       <div className="m-4">
-        <h1 className="text-[#00df9a] text-6xl mt-[50px] font-bold flex justify-center ">
+        <h1 className="text-[#00df9a] text-5xl mt-[50px] font-bold flex justify-center ">
           Ticket Sense
         </h1>
+
         <form
           onSubmit={log}
-          className=" max-w-[700px] mt-[50px] flex flex-col items-center justify-between w-full mx-auto "
+          className=" max-w-[700px] mt-[80px]  flex flex-col items-center justify-center w-full mx-auto "
         >
-          <div className="flex flex-row w-full">
-            <input
-              onChange={(e) => setMovie(e.target.value)}
-              className="p-3 flex w-full rounded-l-md  bg-gray-600  text-white"
-              type="text"
-              placeholder="Movie Name"
-            />
-            <input
-              onChange={(e) => setTheater(e.target.value)}
-              className="p-3 flex w-full rounded-r-md border-l-2 border-gray-800 bor bg-gray-600 text-white"
-              type="text"
-              placeholder="Movie Theater"
-            />
-          </div>
-          <div className="flex flex-col justify-between w-full items-center md:flex-row">
-            <div>
-              <p className="mt-8 mb-3 text-gray-300 flex justify-center items-center font-semibold">
-                Which website?
-              </p>
-              <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={{
-                  colorScheme: "dark",
-                  colors: {
-                    // override dark colors to change them for all components
-                    dark: [
-                      "#d5d7e0",
-                      "#acaebf",
-                      "#8c8fa3",
-                      "#666980",
-                      "#4d4f66",
-                      "#1d1e30",
-                      "#0c0d21",
-                      "#01010a",
-                    ],
-                  },
-                }}
-              >
-                <Chips
-                  size="md"
-                  multiple={false}
-                  value={value}
-                  onChange={setSite}
-                >
-                  <Chip value="bms">BookMyShow</Chip>
-                  <Chip value="tk">Ticket New</Chip>
-                </Chips>
-              </MantineProvider>
-            </div>
+          <div className=" mb-3 flex flex-col sm:flex-row justify-between items-center">
+            <p className="  text-gray-300  m-2 ">Select Booking Website</p>
 
-            <div className="mb-5">
-              <p className="mt-8 mb-3 text-gray-300 flex justify-center items-center font-semibold">
-                Choose Ticket Booking Date(s)
-              </p>
-              <MantineProvider
-                withGlobalStyles
-                withNormalizeCSS
-                theme={{
-                  colorScheme: "dark",
-                  colors: {
-                    // override dark colors to change them for all components
-                    dark: [
-                      "#d5d7e0",
-                      "#acaebf",
-                      "#8c8fa3",
-                      "#666980",
-                      "#4d4f66",
-                      "#1d1e30",
-                      "#0c0d21",
-                      "#01010a",
-                    ],
-                  },
-                }}
-              >
-                <Calendar multiple value={value} onChange={setValue} />
-              </MantineProvider>
-            </div>
+            <SegmentedControl
+              className="m-2"
+              color="blue"
+              value={site}
+              onChange={setSite}
+              data={[
+                { label: "BookMyShow", value: "bms" },
+                { label: "TicketNew", value: "tk" },
+              ]}
+            />
           </div>
-          <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] m-4 p-3">
+          <Autocomplete
+            className="w-full px-2 m-2 sm:w-2/3"
+            placeholder="Movie Name"
+            required
+            icon={<Movie size={16} />}
+            value={movie}
+            onChange={setMovie}
+            data={moviedata}
+          />
+          <Autocomplete
+            className="w-full px-2 m-2 sm:w-2/3"
+            placeholder="Movie Theater"
+            required
+            icon={<MasksTheater size={16} />}
+            value={theater}
+            onChange={setTheater}
+            data={["React", "Angular", "Svelte", "Vue"]}
+          />
+
+          <DatePicker
+            className="w-full px-2 m-2 sm:w-2/3"
+            placeholder="Pick date"
+            allowFreeInput
+            inputFormat="DD/MM/YYYY"
+            icon={<Calendar size={16} />}
+            value={value}
+            onChange={onChange}
+          />
+
+          <button className="bg-[#00df9a] text-black rounded-md font-medium w-[200px] m-4 mt-8 p-3">
             Enable Ticket Sense
           </button>
         </form>
--- tswfrontend/src/pages/Home.jsx
+++ tswfrontend/src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Main from "../components/Main";
+import { MantineProvider } from "@mantine/core";
 
 const Home = () => {
   return (
     <>
-      <Main />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <Main />
+      </MantineProvider>
     </>
   );
 };
