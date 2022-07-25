8fff38d5fe9a7f4dde15a941c04168f217a96aa9
Annlee Fores <annleefores@gmail.com>
2022-07-25 22:03:58 +0530

    finished setting up bms and tktnew location

--- tswfrontend/src/components/Main.jsx
+++ tswfrontend/src/components/Main.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { DatePicker } from "@mantine/dates";
-import { SegmentedControl, Autocomplete } from "@mantine/core";
+import { SegmentedControl, Autocomplete, Select } from "@mantine/core";
 import dayjs from "dayjs";
-import { Calendar, Movie } from "tabler-icons-react";
+import { Calendar, Location, MasksTheater, Movie } from "tabler-icons-react";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-
 import axios from "axios";
-import Theatre from "./Theatre";
+import jsonp from "jsonp";
 
 dayjs.extend(customParseFormat);
 
@@ -15,11 +14,27 @@ const Main = () => {
   const [site, setSite] = useState("bms");
   const [movie, setMovie] = useState("");
   const [moviedata, setMoviedata] = useState([]);
-
   const [value, onChange] = useState(new Date());
+
+  const [location, setLocation] = useState(
+    '{ "name": " ", "location_code": " " }'
+  );
+  const [locdata, setLocdata] = useState([]);
+  const [theater, setTheater] = useState(
+    `{ "name": " ", "theatre_code": " " }`
+  );
+  const [theaterdata, setTheaterdata] = useState([]);
 
   // convert output from calendar to date format
   const dateFormat = dayjs(value).format("YYYY-MM-DD");
+
+  useEffect(() => {
+    if (site === "bms") {
+      bms_sense();
+    } else {
+      tk_sense();
+    }
+  }, [site]);
 
   useEffect(() => {
     if (movie.length > 1) {
@@ -43,15 +58,89 @@ const Main = () => {
     }
   }, [movie]);
 
+  const bms_sense = async () => {
+    axios
+      .get(`https://in.bookmyshow.com/api/explore/v1/discover/regions`)
+      .then((response) => {
+        const location_data = response.data.BookMyShow.TopCities.map(
+          (item, id) => ({
+            key: `t-${id}`,
+            label: item?.RegionName,
+            value: `{"name": "${item?.RegionName}", "location_code": "${item?.RegionCode}"}`,
+          })
+        );
+        response.data.BookMyShow.OtherCities.forEach((element, id) => {
+          location_data.push({
+            key: `o-${id}`,
+            label: element?.RegionName,
+            value: `{"name": "${element?.RegionName}", "location_code": "${element?.RegionCode}"}`,
+          });
+        });
+        setLocdata(location_data);
+      });
+  };
+
+  const tk_sense = async () => {
+    // the tktnew api response is in jsonp format -> jsonp module used to extract infromation
+    jsonp(
+      `https://api.ticketnew.com/api?_api_access_key=b1ed36c7bdbe43c1a76d01a6b8ed9c46&_api_name=ticketnew.app.location.allCities&_api_timestamp=1658745104178&_api_version=1.0.0&request={"appDevice":1658745104178,"appVersion":"4.4.8","appPlatform":"H5","appEnv":"PROD","appChannel":"TICKETNEW"}&_api_signature=4ExixovOYhKwnTkuSm5v7p2uh/I=&_api_jsonp_callback=jsonp0`,
+      { name: "jsonp0" },
+      (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const location_data = data.body.data.hots.map((item, id) => ({
+            key: `t-${id}`,
+            label: item.name,
+            value: `{"name": "${item.name}", "location_code": "${item.id}"}`,
+          }));
+          data.body.data.all.forEach((element, id) => {
+            location_data.push({
+              key: `o-${id}`,
+              label: element.name,
+              value: `{"name": "${element.name}", "location_code": "${element.id}"}`,
+            });
+          });
+          setLocdata(location_data);
+        }
+      }
+    );
+  };
+
+  useEffect(() => {
+    const loc = JSON.parse(location);
+    axios
+      .get(
+        `https://in.bookmyshow.com/pwa/api/de/venues?regionCode=${loc.location_code}&eventType=MT`
+      )
+
+      .then((response) => {
+        const theatre_data = response.data.BookMyShow.arrVenue.map(
+          (item, id) => ({
+            key: `T-${id}`,
+            label: item.VenueName,
+            value: `{ "name": "${item.VenueName}", "theatre_code": "${item.VenueCode}" }`,
+          })
+        );
+        setTheaterdata(theatre_data);
+      });
+  }, [location]);
+
   const log = async (e) => {
     e.preventDefault();
 
     console.log(site);
     console.log(dateFormat);
-    console.log(moviedata);
+    console.log(movie);
+    const loc = JSON.parse(location);
+    console.log(loc);
+    const thea = JSON.parse(theater);
+    console.log(thea);
     setMovie("");
     setMoviedata([]);
     onChange(new Date());
+    setLocation('{ "name": " ", "location_code": " " }');
+    setTheater(`{ "name": " ", "theatre_code": " " }`);
   };
 
   return (
@@ -75,7 +164,7 @@ const Main = () => {
               onChange={setSite}
               data={[
                 { label: "BookMyShow", value: "bms" },
-                { label: "TicketNew", value: "tk" },
+                { label: "Ticket New", value: "tk" },
               ]}
             />
           </div>
@@ -89,7 +178,32 @@ const Main = () => {
             onChange={setMovie}
             data={moviedata}
           />
-          <Theatre />
+          <Select
+            className="w-full px-2 m-2 sm:w-2/3"
+            placeholder="Location"
+            required
+            searchable
+            clearable
+            allowDeselect
+            size="md"
+            icon={<Location size={16} />}
+            value={location}
+            onChange={setLocation}
+            data={locdata}
+          />
+          <Select
+            className="w-full px-2 m-2 sm:w-2/3"
+            placeholder="Movie Theater"
+            // required
+            searchable
+            clearable
+            allowDeselect
+            size="md"
+            icon={<MasksTheater size={16} />}
+            value={theater}
+            onChange={setTheater}
+            data={theaterdata}
+          />
 
           <DatePicker
             className="w-full px-2 m-2 sm:w-2/3"
