0d954f344e33254a5ba9a9a853ded17e473a72a9
Annlee Fores <annleefores@gmail.com>
2022-08-02 21:55:07 +0530

    (modified) url structure

--- ticketsense/urls.py
+++ ticketsense/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/verifyuser/", views.verifyUser, name="verifyuser"),
-    path("api/getdata/<str:pk>/", views.getData, name="getData"),
-    path("api/trigger/", views.trigger, name="trigger"),
-    path("api/trigger/<str:pk>/", views.single_trig, name="single_trig"),
-    path("api/tktnew/<str:location>/", views.tktnew_theatre, name="tktnew_theatre"),
-    path("api/bms/<str:location>/", views.bms_theatre, name="bms_theatre"),
+    path("verifyuser/", views.verifyUser, name="verifyuser"),
+    path("getdata/<str:pk>/", views.getData, name="getData"),
+    path("trigger/", views.trigger, name="trigger"),
+    path("trigger/<str:pk>/", views.single_trig, name="single_trig"),
+    path("tktnew/<str:location>/", views.tktnew_theatre, name="tktnew_theatre"),
+    path("bms/<str:location>/", views.bms_theatre, name="bms_theatre"),
 ]
--- ticketsenseweb/urls.py
+++ ticketsenseweb/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ticketsense.urls')),
+    path('api/', include('ticketsense.urls')),
 ]
