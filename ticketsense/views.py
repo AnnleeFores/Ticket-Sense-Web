6ba5838feaf39883f20c182914bfbd3aa79d169c
Annlee Fores <annleefores@gmail.com>
2022-08-05 13:54:03 +0530

    test to see if crawler and celery is working

--- ticketsense/views.py
+++ ticketsense/views.py
@@ -71,10 +71,10 @@ def index(request):
     ]
     
     # for testing purposes only
-    # daily_func.delay()
-    # get_tktnew_data.delay('Calicut')
-    # get_tktnew_data.delay('Peravoor')
-    # get_tktnew_data.delay('Thalassery')
+    daily_func.delay()
+    get_tktnew_data.delay('Calicut')
+    get_tktnew_data.delay('Peravoor')
+    get_tktnew_data.delay('Thalassery')
 
   
     return Response(routes)
