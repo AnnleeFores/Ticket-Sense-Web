103c62ef94b8432b4810637f17c77e782226b6d5
Annlee Fores <annleefores@gmail.com>
2022-08-08 18:46:38 +0530

    .

--- runserver.sh
+++ runserver.sh
@@ -4,5 +4,5 @@
 # python manage.py makemigrations
 # python manage.py migrate
 # gunicorn ticketsenseweb.wsgi --bind=0.0.0.0:80
-celery -A ticketsenseweb worker -l info -n a1
-# celery -A ticketsenseweb.celery beat -l INFO
+# celery -A ticketsenseweb worker -l info -n a1
+celery -A ticketsenseweb.celery beat -l INFO
