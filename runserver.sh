ed7ac2b966e69a223d96c64ae238b8ff25f195aa
Annlee Fores <annleefores@gmail.com>
2022-08-08 19:28:01 +0530

    updated celery

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
-gunicorn ticketsenseweb.wsgi --bind=0.0.0.0:80
-# celery -A ticketsenseweb worker -l info -n a1
+# python manage.py collectstatic --no-input
+# python manage.py makemigrations
+# python manage.py migrate
+# gunicorn ticketsenseweb.wsgi --bind=0.0.0.0:80
+celery -A ticketsenseweb worker -l info -n a1
 # celery -A ticketsenseweb.celery beat -l INFO
