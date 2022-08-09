3a88ec344837f6de4aca1f4af32baae635a4ea61
Annlee Fores <annleefores@gmail.com>
2022-08-09 19:15:52 +0530

    calender bug fix

--- tswfrontend/src/util/PrivateRoutes.js
+++ tswfrontend/src/util/PrivateRoutes.js
@@ -11,7 +11,7 @@ const PrivateRoutes = () => {
 
   let data = sessionStorage.getItem("user");
 
-  return true ? <Outlet /> : <Navigate to="/" />;
+  return data ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoutes;
