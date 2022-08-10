509ef452ae02a0ed96407a113428905e09ab0fde
Annlee Fores <annleefores@gmail.com>
2022-08-10 13:42:53 +0530

    capfile for each part

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# python manage.py collectstatic --no-input
-# python manage.py makemigrations
-# python manage.py migrate
-# gunicorn ticketsenseweb.wsgi --bind=0.0.0.0:80
-# celery -A ticketsenseweb worker -l info -n a1
-# celery -A ticketsenseweb.celery beat -l INFO
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+gunicorn ticketsenseweb.wsgi --bind=0.0.0.0:80
+
