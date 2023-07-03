75a99883384e28e38768384da392f24a11d02bf1
annleefores <annleefores@gmail.com>
2023-07-03 22:12:08 +0530

    tf main

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+
+
+# configure a new store for tf state 
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-ticketsense"
+    key            = "terraform/ticketsense-backend/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "tf-state-ticketsense-lock"
+    encrypt        = true
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "2.4.0"
+    }
+  }
+
+  required_version = "~> 1.5.1"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
