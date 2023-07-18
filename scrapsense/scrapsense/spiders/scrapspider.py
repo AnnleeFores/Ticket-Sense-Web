53940f828e4e362f9cbdad04bfd9fffaa30c7174
annleefores <annleefores@gmail.com>
2023-07-18 15:43:52 +0530

    remove tf code and refactor

--- scrapsense/scrapsense/spiders/scrapspider.py
+++ scrapsense/scrapsense/spiders/scrapspider.py
@@ -10,68 +10,64 @@ import sys
 
 # Prod
 path = os.getcwd()
-sys.path.append((path)) # add working dir to path
+sys.path.append((path))  # add working dir to path
 
-from stringMatching import stripDown
+from lib.stringMatching import stripDown
+
 
 # spider for ticketnew websited
 class tkSpider(scrapy.Spider):
+    name = "tk"
 
-    name = 'tk'
-    
     def start_requests(self):
-        
-        yield scrapy.Request(
-            url=f'{self.link}'
-            )
+        yield scrapy.Request(url=f"{self.link}")
 
     async def parse(self, response):
-
         # values accessed from get url
         film = self.film
         date = self.date
 
         ## if date booking is active get the details
-        active = response.css('li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active').get()
-        
+        active = response.css(
+            "li.ui-tabs-tab.ui-corner-top.ui-state-default.ui-tab.ui-tabs-active.ui-state-active"
+        ).get()
+
         # logic to check and retrive only the needed data in json format
         if active != None:
-            venuehtml =  response.css('div#divTheatreInfo')
-            DT = (parser.parse(date)).strftime('%Y-%m-%d')
-            for show in response.css('div.tn-entity-details')[1:]:
-                showname = show.css('h5::text').get()
+            venuehtml = response.css("div#divTheatreInfo")
+            DT = (parser.parse(date)).strftime("%Y-%m-%d")
+            for show in response.css("div.tn-entity-details")[1:]:
+                showname = show.css("h5::text").get()
                 filmkeyword_val = stripDown(film)
                 film_val = stripDown(showname.lower())
                 fuzz_value = fuzz.token_set_ratio(filmkeyword_val, film_val)
                 print(fuzz_value)
-                if fuzz_value >= 65:  # a threshold value based on Levenshtein distance comparison of 2 strings. Make changes to threshold value to work as you needed
+                if (
+                    fuzz_value >= 65
+                ):  # a threshold value based on Levenshtein distance comparison of 2 strings. Make changes to threshold value to work as you needed
                     yield {
-                        'venue' : venuehtml.css('h2::text').get(),
-                        'show' : showname,
-                        'date' : DT,
+                        "venue": venuehtml.css("h2::text").get(),
+                        "show": showname,
+                        "date": DT,
                     }
-        else: 
-            yield {
-                None
-            }
+        else:
+            yield {None}
 
 
 # spider for sourcing ticketnew booking links and theater lists
 class tkdataSpider(scrapy.Spider):
-    name = 'tkdata'
-    
+    name = "tkdata"
+
     def start_requests(self):
-        
         yield scrapy.Request(
-            url=f'https://www.ticketnew.com/online-advance-booking/Theatres/C/{self.location}',
-            )
+            url=f"https://www.ticketnew.com/online-advance-booking/Theatres/C/{self.location}",
+        )
 
     async def parse(self, response):
-   
-        for elem in response.css('div.tn-entity'):
-            venuename = elem.css('div.tn-entity-details')
-            bookinglink = elem.css('div.tn-entity-book')
+        for elem in response.css("div.tn-entity"):
+            venuename = elem.css("div.tn-entity-details")
+            bookinglink = elem.css("div.tn-entity-book")
             yield {
-                'value' : venuename.css('h5::text').get(),
-                'bookinglink' : bookinglink.css('a').attrib['href'],
+                "value": venuename.css("h5::text").get(),
+                "bookinglink": bookinglink.css("a").attrib["href"],
             }
--- ticketsense/tasks.py
+++ ticketsense/tasks.py
@@ -9,12 +9,15 @@ from thefuzz import fuzz
 
 # for jsonp
 from json import loads
+
 scraper = cloudscraper.create_scraper()
 
 # os module used for getting env variables
 import os
+
 # telegram module to interact with telegram chat app
 import telebot
+
 # used to source data from .env
 from dotenv import load_dotenv
 
@@ -27,9 +30,9 @@ import re
 import os
 import sys
 
-from stringMatching import stripDown
+from lib.stringMatching import stripDown
 
-sys.path.append(os.getcwd()) # add working dir to path
+sys.path.append(os.getcwd())  # add working dir to path
 
 logger = get_task_logger(__name__)
 
@@ -37,43 +40,40 @@ logger = get_task_logger(__name__)
 load_dotenv()
 
 # assigns value collected from .env to variables
-API_KEY_TEST = os.getenv('API_KEY_TEST')
-API_KEY = os.getenv('API_KEY')
-scrapsense_link =  os.getenv('scrapsense_link')
+API_KEY_TEST = os.getenv("API_KEY_TEST")
+API_KEY = os.getenv("API_KEY")
+scrapsense_link = os.getenv("scrapsense_link")
+
 
 # get image from url
 def getImage(img_link):
-
-    URL = f'https://image.tmdb.org/t/p/w500{img_link}'
+    URL = f"https://image.tmdb.org/t/p/w500{img_link}"
 
     with urllib.request.urlopen(URL) as url:
         img = Image.open(url)
     return img
 
 
-
-
-
-
-
 # code to initialize telegram bot function
 bot = telebot.TeleBot(API_KEY)
+
+
 @shared_task(ignore_result=True)
 def message(msg, pk, USER_ID, poster):
     try:
         trigger = Trigger.objects.get(id=pk)
         trigger.delete()
     except:
-        logger.info(f'''delete from DB - data doesn't exist''')
+        logger.info(f"""delete from DB - data doesn't exist""")
 
     try:
         if not poster:
-            bot.send_message(USER_ID, msg, parse_mode= 'Markdown')
+            bot.send_message(USER_ID, msg, parse_mode="Markdown")
         else:
-            bot.send_photo(USER_ID, getImage(poster), msg, parse_mode= 'Markdown')
+            bot.send_photo(USER_ID, getImage(poster), msg, parse_mode="Markdown")
     except:
-        logger.info('issue with telegram user permissions')
-    
+        logger.info("issue with telegram user permissions")
+
     # for i in range(3):
     #     try:
     #         if not poster:
@@ -83,120 +83,128 @@ def message(msg, pk, USER_ID, poster):
     #     except:
     #         logger.info('issue with telegram user permissions')
     #     sleep(60)
-    
+
 
 # testbot = telebot.TeleBot(API_KEY_TEST)
 # def testmessage(msg):
 #     testbot.send_message(USER_ID, msg)
 
+
 @shared_task(ignore_result=True)
 def daily_func():
+    response = requests.get(
+        f'https://api.ticketnew.com/api?_api_access_key=b1ed36c7bdbe43c1a76d01a6b8ed9c46&_api_name=ticketnew.app.location.allCities&_api_timestamp=1658745104178&_api_version=1.0.0&request={{"appDevice":1658745104178,"appVersion":"4.4.8","appPlatform":"H5","appEnv":"PROD","appChannel":"TICKETNEW"}}&_api_signature=4ExixovOYhKwnTkuSm5v7p2uh/I=&_api_jsonp_callback=jsonp0'
+    )
 
-    response = (requests.get(f'https://api.ticketnew.com/api?_api_access_key=b1ed36c7bdbe43c1a76d01a6b8ed9c46&_api_name=ticketnew.app.location.allCities&_api_timestamp=1658745104178&_api_version=1.0.0&request={{"appDevice":1658745104178,"appVersion":"4.4.8","appPlatform":"H5","appEnv":"PROD","appChannel":"TICKETNEW"}}&_api_signature=4ExixovOYhKwnTkuSm5v7p2uh/I=&_api_jsonp_callback=jsonp0'))
-    
-    startidx = response.text.find('(')
-    endidx = response.text.rfind(')')
-    data = loads(response.text[startidx + 1:endidx])
+    startidx = response.text.find("(")
+    endidx = response.text.rfind(")")
+    data = loads(response.text[startidx + 1 : endidx])
 
-    for j in data['body']['data']['hots']:
-        get_tktnew_data.delay(j['name'])
-    
-    for i in data['body']['data']['all']:
-        get_tktnew_data.delay(i['name'])
-    
-    
+    for j in data["body"]["data"]["hots"]:
+        get_tktnew_data.delay(j["name"])
+
+    for i in data["body"]["data"]["all"]:
+        get_tktnew_data.delay(i["name"])
+
+
 @shared_task(ignore_result=True)
 def get_tktnew_data(location):
-
-    response = (requests.get(f'{scrapsense_link}/crawl.json?spider_name=tkdata&start_requests=true&crawl_args={{"location":"{location}"}}').json())
+    response = requests.get(
+        f'{scrapsense_link}/crawl.json?spider_name=tkdata&start_requests=true&crawl_args={{"location":"{location}"}}'
+    ).json()
     try:
-        data = response['items']
+        data = response["items"]
     except:
-        data = ''
+        data = ""
 
     store_tktnew_data.delay(data, location)
 
 
 @shared_task(ignore_result=True)
 def store_tktnew_data(response, location):
-    tktnewData, created = TktnewData.objects.update_or_create(location=location, data=response, defaults={'location': location})
+    tktnewData, created = TktnewData.objects.update_or_create(
+        location=location, data=response, defaults={"location": location}
+    )
     # tktnewData = TktnewData.objects.create(location=location, data=response)
-
-
 
 
 @shared_task(ignore_result=True)
 def five_min_func():
-
     try:
         triggers = Trigger.objects.all()
 
         for trigger in triggers:
             pk = trigger.id
             link = trigger.link
-            filmkeyword = (trigger.movie).lower() #make sure that this is lowercase otherwise the result will be incorrect
-            date = (trigger.date).strftime('%Y-%m-%d')
+            filmkeyword = (
+                trigger.movie
+            ).lower()  # make sure that this is lowercase otherwise the result will be incorrect
+            date = (trigger.date).strftime("%Y-%m-%d")
             site = trigger.site
-            USER_ID = trigger.tg_user.id #from foreign key data
+            USER_ID = trigger.tg_user.id  # from foreign key data
             poster = trigger.poster
             try:
                 venuecode = trigger.theater_code
             except:
-                venuecode = ''
+                venuecode = ""
             fetch.delay(link, filmkeyword, date, site, pk, USER_ID, poster, venuecode)
     except:
         pass
 
-        
 
 @shared_task(ignore_result=True)
 def fetch(link, filmkeyword, date, site, pk, USER_ID, poster, venuecode):
-
     data = []
-    
-    if site == 'tk':
-        response = (requests.get(f'{scrapsense_link}/crawl.json?spider_name=tk&start_requests=true&crawl_args={{"link":"{link}","film":"{filmkeyword}","date":"{date}"}}').json())
+
+    if site == "tk":
+        response = requests.get(
+            f'{scrapsense_link}/crawl.json?spider_name=tk&start_requests=true&crawl_args={{"link":"{link}","film":"{filmkeyword}","date":"{date}"}}'
+        ).json()
         try:
-            data = response['items']
+            data = response["items"]
         except:
             data
     else:
-        date = re.sub(r'[^\w]', '', date)
-        
-        response = (scraper.get(f'https://in.bookmyshow.com/api/v2/mobile/showtimes/byvenue?venueCode={venuecode}&dateCode={date}').json())
+        date = re.sub(r"[^\w]", "", date)
+
+        response = scraper.get(
+            f"https://in.bookmyshow.com/api/v2/mobile/showtimes/byvenue?venueCode={venuecode}&dateCode={date}"
+        ).json()
         try:
             bmsdata = response
         except:
-            bmsdata = ''
-        
-        if bmsdata['ShowDetails'][0]['Date'] == date and bmsdata != '':
-            for films in bmsdata['ShowDetails'][0]['Event']:
-                film = films['ChildEvents'][0]['EventName']
-                 
+            bmsdata = ""
+
+        if bmsdata["ShowDetails"][0]["Date"] == date and bmsdata != "":
+            for films in bmsdata["ShowDetails"][0]["Event"]:
+                film = films["ChildEvents"][0]["EventName"]
+
                 filmkeyword_val = stripDown(filmkeyword)
                 film_val = stripDown(film.lower())
 
                 fuzz_value = fuzz.token_set_ratio(filmkeyword_val, film_val)
                 logger.info(fuzz_value)
-         
-                if fuzz_value >= 65:  # a threshold value based on Levenshtein distance comparison of 2 strings. Make changes to threshold value to work as you needed
-                    date = bmsdata['ShowDetails'][0]['Date']
-                    venue = bmsdata['ShowDetails'][0]['Venues']['VenueName']
-                    jsondata = {'venue':venue, 'show':film, 'date':date}
+
+                if (
+                    fuzz_value >= 65
+                ):  # a threshold value based on Levenshtein distance comparison of 2 strings. Make changes to threshold value to work as you needed
+                    date = bmsdata["ShowDetails"][0]["Date"]
+                    venue = bmsdata["ShowDetails"][0]["Venues"]["VenueName"]
+                    jsondata = {"venue": venue, "show": film, "date": date}
                     data.append(jsondata)
 
     if data != []:
         for i in data:
-            film = i['show']
-            venue = i['venue']
-            date = (parser.parse(i['date'])).strftime('%d-%m-%Y')
+            film = i["show"]
+            venue = i["venue"]
+            date = (parser.parse(i["date"])).strftime("%d-%m-%Y")
 
-            if site == 'bms':
-                websitelink = f'https://in.bookmyshow.com/buytickets/{link}'
+            if site == "bms":
+                websitelink = f"https://in.bookmyshow.com/buytickets/{link}"
             else:
-                websitelink = f'{link}'
+                websitelink = f"{link}"
 
-            #** ** to make text bold for telegram based on markdown parsing
+            # ** ** to make text bold for telegram based on markdown parsing
             msg = f""" *Ticket Sense* found ticket booking
 
 Movie:
@@ -210,10 +218,8 @@ Date:
 
 Link: {websitelink} 
 
-_Make sure the booking date is correct when buying the ticket._ """  
+_Make sure the booking date is correct when buying the ticket._ """
 
             message.delay(msg, pk, USER_ID, poster)
 
-    return 'done'
-
-
+    return "done"
